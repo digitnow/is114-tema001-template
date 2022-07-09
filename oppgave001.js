31d10213ae0b7282eba29417c80b042684e11386
Janis Gailis <janis.gailis@uia.no>
2022-07-09 09:45:08 +0200

    Oppgave 1 test

---
+++ oppgave001.js
@@ -0,0 +1,11 @@
+// oppgave001.js IS-114 Tema001
+  Hva er typen til variablene definert under? 
+  Bevis ved å bruke det reserverte ordet i JavaScript typeof 
+  og skriv ut resultater til konsollen. 
+*/
+
+let str1 = 'Daily'; 
+let str2 = "Mirror"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
